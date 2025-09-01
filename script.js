bccb999dbba475bf4ce387b619042b817dcabe7a
zDaggerz <felipelealrocha1@gmail.com>
2025-09-01 20:43:36 -0300

    Delete script.js
    Signed-off-by: zDaggerz <felipelealrocha1@gmail.com>

--- script.js
+++
@@ -1,8 +0,0 @@
-// A palavra 'const' é usada para declarar uma constante
-const botaoEnviar = document.getElementById('btn-enviar');
-
-// Adiciona um "ouvinte de evento" que espera por um clique no botão
-botaoEnviar.addEventListener('click', function() {
-    // O método alert() exibe uma caixa de diálogo de alerta para o usuário
-    alert('Mensagem enviada com sucesso! (Esta é uma simulação)');
-});
